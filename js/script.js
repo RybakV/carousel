639d7c66a6cc1eb2dfb5ffab2e4ef5c4d02e6677
Viacheslav <viacheslav.rybak.pb@gmail.com>
2023-02-03 03:06:16 +0200

    MVP slider

--- js/script.js
+++ js/script.js
@@ -1,38 +1,59 @@
-
+const itemsCount = 6;
+const apiKey = '2tYsoxOURyXc9UcVQb7n3R8O12OXTKJTzDo_IoYyRNI';
+const collectionId = 'CpdiISY2JKQ'
 const carousel = document.querySelector("#carousel");
+const navPrev = document.querySelector(".nav-prev");
+const navNext = document.querySelector(".nav-next");
+let carouselContent = '';
 
-fetch('https://pixabay.com/api/?key=18618763-c9ef92b1310e6ed5c6aaa8e00&q=yellow+flowers&image_type=photo&page=1&per_page=3')
+fetch(`https://api.unsplash.com/collections/${collectionId}/photos?client_id=${apiKey}&per_page=${itemsCount}`)
     .then(response => response.json())
-    .then(json => {
-        console.log(json);
-        carousel.innerHTML += `<img src="${json.hits[0].webformatURL}" alt="${json.hits[0].tags} by ${json.hits[0].user}"/>`;
+    .then(json => renderSlides(json));
+
+function renderCarousel(){
+    carousel.setAttribute('style',`width: ${100 * itemsCount}%; left:0;` )
+}
+function renderSlides(json){
+    renderCarousel();
+    json.forEach(function callback(item, i){
+        carouselContent +=
+            `<div class="item item-${i}" style="background-image: url('${item.urls.regular}')">
+                    <div class="item-copy">
+                        <div class="description">${(item.description ? item.description : 'Nice picture')}</div>
+                        <div class="username">by ${(item.user.username ? item.user.username : 'someone')}</div>
+                    </div>
+                </div>`;
+        carousel.innerHTML = carouselContent;
     })
+}
 
-let f = {
-    "total":33875,
-    "totalHits":500,
-    "hits":[{
-        "id":2295434,
-        "pageURL":"https://pixabay.com/photos/spring-bird-bird-tit-spring-blue-2295434/",
-        "type":"photo",
-        "tags":"spring bird, bird, tit",
-        "previewURL":"https://cdn.pixabay.com/photo/2017/05/08/13/15/spring-bird-2295434_150.jpg",
-        "previewWidth":150,
-        "previewHeight":99,
-        "webformatURL":"https://pixabay.com/get/gfa5928926e5acb4b2b674a8b371531a3697b4c7378c0425c1b3bd2d59a36c82950d089d0ee2df2e65c389cb17aeb3d7d8f2e9ad7f1d4c7f5284a86257dcaf30b_640.jpg","webformatWidth":640,"webformatHeight":426,"largeImageURL":"https://pixabay.com/get/g3a2c59045424764daa54b684f28275bfaa6f20221a63e35ccf9ff68639dc3d00e6bea683900136808952bcf8932c169cbd09a0868f725af4406cf2ff8f71b5a0_1280.jpg",
-        "imageWidth":5363,
-        "imageHeight":3575,
-        "imageSize":2938651,
-        "views":622330,
-        "downloads":358782,
-        "collections":2054,
-        "likes":1982,
-        "user_id":334088,
-        "user":"JillWellington",
-        "userImageURL":"https://cdn.pixabay.com/user/2018/06/27/01-23-02-27_250x250.jpg"
-    },
-]}
+navPrev.addEventListener('click',scrollSlider);
+navNext.addEventListener('click',scrollSlider);
 
-
-
-
+function scrollSlider(){
+    function removeForbiddenCharacters(input) {
+        const forbiddenChars = ['%','px',]
+        for (let char of forbiddenChars) {
+            input = input.split(char).join('');
+        }
+        return input
+    }
+    let positionLeft = removeForbiddenCharacters(carousel.style.left);
+    let newPositionLeft = 0;
+    if (this.classList.contains('nav-prev')){
+        if (positionLeft >= 0){
+            newPositionLeft = (itemsCount-1) * (-100) + '%';
+        }
+        else {
+            newPositionLeft = (Number(positionLeft) + 100)+'%';
+        }
+    }
+    else if (this.classList.contains('nav-next')){
+        if (positionLeft <= (itemsCount-1) * (-100)){
+        }
+        else {
+            newPositionLeft = (Number(positionLeft) - 100)+'%';
+        }
+    }
+    carousel.style.left = newPositionLeft;
+}
